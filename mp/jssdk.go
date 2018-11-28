c8af5e9ed110437d056a41011d7445ab9c2cd72b
ON10N <zhyc9de@hotmail.com>
2018-11-28 10:35:49 +0800

    新增接口权限

--- mp/jssdk.go
+++ mp/jssdk.go
@@ -57,6 +57,7 @@ const ( // api 权限列表
 	ApiAddCard                             = "addCard"
 	ApiChooseCard                          = "chooseCard"
 	ApiOpenCard                            = "openCard"
+	ApiOpenAddress                         = "openAddress"
 )
 
 // https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
